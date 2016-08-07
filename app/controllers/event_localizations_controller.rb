96882811997596022c77d7e7fa3ea68ed83d4e51
Emmanuel Mora Marin <emmanuelmora05@gmail.com>
2016-08-07 11:34:51 -0600

    Fixed dBs regexp.

--- app/controllers/event_localizations_controller.rb
+++ app/controllers/event_localizations_controller.rb
@@ -8,22 +8,21 @@ class EventLocalizationsController < ApplicationController
 
     # Filters wireless status that begins with word ftw
     ssids = {}
-    event_localization_params[:wireless_status].split("\n").each do |line|
+    @_params[:wireless_status].split("\n").each do |line|
       args = line.split(',')
-      if ['FTW1','FTW2','FTW3'].contains(args[1])
+      if ['"FTW1"','"FTW2"','"FTW3"'].include?(args[1])
         ssids[args[1]] = args[2]
       end
     end
 
-    params = [
-        distance_point_a: ssids['FTW1'],
-        distance_point_b: ssids['FTW2'],
-        distance_point_c: ssids['FTW3'],
-        wireless_uuid: event_localization_params[:wireless_uuid]
-    ]
 
-    @event_localization = EventLocalization.create(params)
-    @event_localization.wristband_uuid = Wristband.find_by_wristband_uuid(event_localization_params[:wristband_uuid])
+    @event_localization = EventLocalization.create(
+        distance_point_a: ssids['"FTW1"'],
+        distance_point_b: ssids['"FTW2"'],
+        distance_point_c: ssids['"FTW3"'],
+        wristband_uuid: event_localization_params[:id]
+    )
+    @event_localization.wristband_uuid = Wristband.find_by_wristband_uuid(event_localization_params[:id])
     @event_localization.calculate_indoor_coordinates
     @event_localization.save!
 
