7ab3f9f589746287796bb2efb2ee064ebdd3d9e6
Emmanuel Mora Marin <emmanuelmora05@gmail.com>
2016-08-07 16:32:57 -0600

    Tested with demo environment.

--- app/controllers/event_localizations_controller.rb
+++ app/controllers/event_localizations_controller.rb
@@ -10,29 +10,34 @@ class EventLocalizationsController < ApplicationController
     ssids = {}
     @_params[:wireless_status].split("\n").each do |line|
       args = line.split(',')
-      if ['"FTW1"','"FTW2"','"FTW3"'].include?(args[1])
+      if ['"FTW1FTW1"','"FTW2FTW2"','"FTW3FTW3"'].include?(args[1])
 
         # calculate linear distance from dbm's
-        dbm = args[2]
+        dbm = args[2].to_f
         distance = decibels_intensity_to_meters(dbm)
         ssids[args[1]] = distance
       end
     end
 
-
     @event_localization = EventLocalization.create(
-        distance_point_a: ssids['"FTW1"'],
-        distance_point_b: ssids['"FTW2"'],
-        distance_point_c: ssids['"FTW3"'],
+        distance_point_a: ssids['"FTW1FTW1"'],
+        distance_point_b: ssids['"FTW2FTW2"'],
+        distance_point_c: ssids['"FTW3FTW3"'],
         wristband_uuid: event_localization_params[:id]
     )
+
+    logger.info @event_localization.to_json
+
     @event_localization.wristband_uuid = Wristband.find_by_wristband_uuid(event_localization_params[:id])
-    @event_localization.calculate_indoor_coordinates
+    @event_localization.calculate_indoor_coordinates()
     @event_localization.save!
 
     respond_to do |format|
       # FIXME: Logic that defines matches leds according to distances.
-      format.json{ render json: { led_status:'match' } }
+      format.json{ render json: {
+          led_status:'match',
+          coords:{x:@event_localization.coord_x, y:@event_localization.coord_y}
+      } }
     end
   end
 
@@ -47,8 +52,11 @@ class EventLocalizationsController < ApplicationController
     end
 
     def decibels_intensity_to_meters( dbm )
-      10 ** ( ( 27.55 - ( 20*Math.log10(2412) ) + dbm ) / 20 )
+      10 ** ( ( 27.55 - ( 20*Math.log10(2412) ) + dbm.abs ) / 20 )
     end
-end
+
+    def decibels_intensity_to_meters( dbm )
+      10 ** ( ( 27.55 - ( 20*Math.log10(2412) ) + dbm.abs ) / 20 )
+    end
 
 end
--- app/models/event_localization.rb
+++ app/models/event_localization.rb
@@ -9,7 +9,8 @@ class EventLocalization < ActiveRecord::Base
     self.real_distance_b = self.distance_point_b #10 ** self.distance_point_b
     self.real_distance_c = self.distance_point_c #10 ** self.distance_point_c
 
-    point = get_coords_in_grid_with_angles(EventLocalization.reference_nodes[0],
+    point = get_coords_in_grid_with_angles(
+                                   EventLocalization.reference_nodes[0],
                                    EventLocalization.reference_nodes[1],
                                    EventLocalization.reference_nodes[2],
                                    real_distance_a,
@@ -20,7 +21,8 @@ class EventLocalization < ActiveRecord::Base
   end
 
   def self.reference_nodes
-    return [ {x:55,y:10}, {x:10,y:110}, {x:110,y:110} ]
+    #return [ {x:0,y:0}, {x:10.44,y:0}, {x:6.67,y:2.9} ]
+    return [ {x:0,y:0}, {x:6.96,y:0}, {x:8.12,y:4.93} ]
   end
 
   private
@@ -31,7 +33,7 @@ class EventLocalization < ActiveRecord::Base
       s =  1
 
       # first point
-      p_0 = { x: (ref1[:x] + ref2[:x] + ref3[:x]) / 3, y:(ref1[:y] + ref2[:y] + ref3[:y] / 3) }
+      p_0 = { x: ((ref1[:x] + ref2[:x] + ref3[:x]) / 3), y:((ref1[:y] + ref2[:y] + ref3[:y]) / 3) }
       c_0 = cost_function(ref1, ref2, ref3, p_0, d1, d2, d3  )
 
       # first possible directions to step ahead.
@@ -69,14 +71,14 @@ class EventLocalization < ActiveRecord::Base
             dy: s * dy/Math.sqrt(dx**2 + dy**2)
         }
 
-        puts "point:#{target_point} cost:#{target_point_cost} #{iteration} #{diff}"
+        logger.info "point:#{target_point} cost:#{target_point_cost} #{iteration} #{diff}"
 
         new_target_point = { x:target_point[:x] + diff[:dx], y:target_point[:y] + diff[:dy] }
         new_target_point_cost = cost_function(ref1, ref2, ref3, new_target_point, d1, d2, d3  )
 
         # if we reached a minimum, stop iterating
         if new_target_point_cost >= target_point_cost
-          puts "stopped at point:#{new_target_point} cost:#{new_target_point_cost} #{iteration} #{diff}"
+          logger.info "stopped at point:#{new_target_point} cost:#{new_target_point_cost} #{iteration} #{diff}"
           break
         end
 
@@ -95,24 +97,24 @@ class EventLocalization < ActiveRecord::Base
     end
 
 
-      def derivate_error_step_y( ref1,ref2,ref3, point, d1,d2,d3 )
-        return 2*distance_points_squared( ref1, point, d1 )*2*(ref1[:y]-d1) + \
-          2*distance_points_squared( ref2, point, d2 )*2*(ref2[:y]-d2) + \
-          2*distance_points_squared( ref3, point, d3 )*2*(ref3[:y]-d3)
-      end
+    def derivate_error_step_y( ref1,ref2,ref3, point, d1,d2,d3 )
+      return 2*distance_points_squared( ref1, point, d1 )*2*(ref1[:y]-d1) + \
+        2*distance_points_squared( ref2, point, d2 )*2*(ref2[:y]-d2) + \
+        2*distance_points_squared( ref3, point, d3 )*2*(ref3[:y]-d3)
+    end
 
-      def cost_function(ref1, ref2, ref3, point, d1, d2, d3 )
-        return distance_points_squared( ref1, point, d1 )**2 \
-          + distance_points_squared( ref2, point, d2 )**2 \
-          + distance_points_squared( ref3, point, d3 )**2
-      end
+    def cost_function(ref1, ref2, ref3, point, d1, d2, d3 )
+      return distance_points_squared( ref1, point, d1 )**2 \
+        + distance_points_squared( ref2, point, d2 )**2 \
+        + distance_points_squared( ref3, point, d3 )**2
+    end
 
-      def distance_points_squared( ref, point, d )
-        return ( ref[:x] - point[:x] )**2 +  ( ref[:y] - point[:y] )**2  - d ** 2
-      end
+    def distance_points_squared( ref, point, d )
+      return ( ref[:x] - point[:x] )**2 +  ( ref[:y] - point[:y] )**2  - d ** 2
+    end
 
-     def distance_points( ref, point, d )
-       return ( ref[:x] - point[:x] ) +  ( ref[:y] - point[:y] ) - d
-     end
+   def distance_points( ref, point, d )
+     return ( ref[:x] - point[:x] ) +  ( ref[:y] - point[:y] ) - d
+   end
 
 end
